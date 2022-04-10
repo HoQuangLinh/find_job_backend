644a444677006a370258f4a759b1bbb12e0a04ef
Ho Quang Linh <hoquanglinh2710@gmail.com>
2022-04-10 09:24:06 +0700

    connect db, add User model

--- app.js
+++ app.js
@@ -3,6 +3,15 @@ const app = express();
 const mongoose = require("mongoose");
 const morgan = require("morgan");
 
+const swaggerUi = require("swagger-ui-express")
+swaggerDocument = require("./swagger.json");
+
+//Add swagger
+app.use(
+    '/api-doc',
+    swaggerUi.serve, 
+    swaggerUi.setup(swaggerDocument)
+  );
 //Log web
 app.use(morgan("tiny"));
 
@@ -17,8 +26,23 @@ app.use(express.json()); //to support JSON encode
 var cors = require("cors");
 app.use(cors());
 app.get("/", (req, res) => res.send("Hello from homepage"));
+
+//Routes app
+var usersRouter=require("./routes/usersRouter")
+app.use("/api/users",usersRouter);
+
+
+//Connect to database
+mongoose
+  .connect(process.env.connectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(function (result) {
+    console.log("Database is connected");
+  })
+  .catch((err) => console.log(err));
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, function () {
-  
   console.log("Server Running on port", PORT);
 });
